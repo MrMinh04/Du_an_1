dd07825e36ee59ab963b0d0c0824f958a0502834
MrMinh04 <nguyenminhxp2004@gmail.com>
2024-07-19 00:01:57 +0700

    hoan_thanh

---
+++ admin/index.php
@@ -0,0 +1,190 @@
+<?php
+include("../model/pdo.php");
+include("header.php");
+include("../model/danhmuc.php");
+include("../model/sanpham.php");
+include("../model/khac.php");
+include("../model/account.php");
+
+
+
+if(isset($_GET["act"])){
+    $act=$_GET["act"];
+    switch($act){
+
+
+
+//DANH MỤC -----------------------------------------------------------------------
+
+        case "list_danh_muc":
+            $category = display_danhmuc();
+            include("danhmuc/list.php");
+            break;
+        case "add_danh_muc":
+            if (isset($_POST["add_category"]) && $_POST["add_category"]){
+                $category_id = $_POST["category_id"];
+                $category_name = $_POST["category_name"];
+                insert_danhmuc($category_id,$category_name);
+                $thongbao = "Thêm thành công";
+            }
+            include("danhmuc/add.php");
+            break;
+
+        case "delete_danh_muc":
+            if (isset($_GET["category_id"]) && $_GET["category_id"]>0){
+            $category_id = $_GET['category_id'];
+            delete_danh_muc($category_id);
+            }
+            $category = display_danhmuc();
+            include("danhmuc/list.php");
+            break;
+        case "edit_danh_muc":
+            if (isset($_GET["category_id"]) && $_GET["category_id"]>0){
+                $category_id = $_GET['category_id'];
+                $update = load_1_danh_muc($category_id);
+            }
+            include("danhmuc/edit.php");
+            break;
+        case "update_danh_muc":
+            if (isset($_POST["update_category"]) && $_POST["update_category"]){
+                $category_name = $_POST["category_name"];
+                $hiden = $_POST["hiden"];
+                update_danh_muc($category_name,$hiden);
+            }
+            $category = display_danhmuc();
+            include("danhmuc/list.php");
+            break;
+
+
+
+// SẢN PHẨM--------------------------------------------------------------------------------------
+
+        case "list_san_pham":
+            if (isset($_POST["kw"]) && $_POST["kw"]!=""){
+                $kw = $_POST['kw'];
+            }else{
+                $kw = "";
+            }
+            $product_view = display_sanpham_view($kw);
+            include "sanpham/list.php";
+            break;    
+        case "add_san_pham":
+            $category = display_danhmuc();
+            if (isset($_POST["add_product"]) && $_POST["add_product"]){
+                $product_id = $_POST["product_id"];
+                $product_name = $_POST["product_name"];
+                $product_price = $_POST["product_price"];
+                $product_describe = $_POST["product_describe"];
+                $product_quantity = $_POST["product_quantity"];
+                $product_category = $_POST["product_category"];
+                //Lấy file
+                $file = $_FILES['product_image'];
+                //lấy tên file
+                $product_image = $file['name'];
+
+                $file2 = $_FILES['product_image2'];
+                //lấy tên file
+                $product_image2 = $file2['name'];
+
+                $file3 = $_FILES['product_image3'];
+                //lấy tên file
+                $product_image3 = $file3['name'];
+
+                $file4 = $_FILES['product_image4'];
+                //lấy tên file
+                $product_image4 = $file4['name'];
+                //Upload
+                move_uploaded_file($file['tmp_name'], "../images/" . $product_image);
+                move_uploaded_file($file['tmp_name'], "../images/" . $product_image2);
+                move_uploaded_file($file['tmp_name'], "../images/" . $product_image3);
+                move_uploaded_file($file['tmp_name'], "../images/" . $product_image4);
+                insert_sanpham($product_id,$product_name,$product_image,$product_image2,$product_image3,$product_image4,$product_price,$product_describe,$product_quantity,$product_category);
+                $thongbao = "Thêm thành công";
+            }
+            include("sanpham/add.php");
+            break;
+        case "delete_san_pham":
+            if (isset($_GET["product_id"]) && $_GET["product_id"]>0) {
+            $product_id = $_GET['product_id'];
+            delete_san_pham($product_id);
+            }
+            if (isset($_POST["kw"]) && $_POST["kw"]!=""){
+                $kw = $_POST['kw'];
+            }else{
+                $kw = "";
+            }
+            $product_view = display_sanpham_view($kw);
+            include "sanpham/list.php";
+            break;  
+            
+        case "edit_san_pham":
+            $category = display_danhmuc();
+            if (isset($_GET["product_id"]) && $_GET["product_id"]>0){
+                $product_id = $_GET['product_id'];
+                $update = load_1_san_pham($product_id);
+            }
+            include("sanpham/edit.php");
+            break;
+        case "update_san_pham":
+            if (isset($_POST["update_product"]) && $_POST["update_product"]){
+                $product_name = $_POST["product_name"];
+                $product_price = $_POST["product_price"];
+                $product_describe = $_POST["product_describe"];
+                $product_quantity = $_POST["product_quantity"];
+                $product_category = $_POST["product_category"];
+                $product_id = $_POST["product_id"];
+                //Lấy file
+                $file = $_FILES['product_image'];
+                //lấy tên file
+                $product_image = $file['name'];
+                //Upload
+                move_uploaded_file($file['tmp_name'], "../images/" . $product_image);
+                update_san_pham($product_id,$product_name,$product_price,$product_describe,$product_quantity,$product_category,$product_image);
+            }
+            if (isset($_POST["kw"]) && $_POST["kw"]!=""){
+                $kw = $_POST['kw'];
+            }else{
+                $kw = "";
+            }
+            $product_view = display_sanpham_view($kw);
+            include "sanpham/list.php";
+            break;  
+
+//Đơn hàng
+        case "list_don_hang":
+            $order = display_don_hang();
+            include("donhang/list.php");
+            break;
+        
+
+
+//Tài khoản
+        case "account":
+            $account = select_account();
+            include("account/list.php");
+            break;
+        case "delete_account":
+            if (isset($_GET["account_id"]) && $_GET["account_id"]>0){
+            $account_id = $_GET['account_id'];
+            delete_account($account_id);
+            }
+            $account = select_account();
+            include("account/list.php");
+            break;
+
+
+//Thống kê
+        case "thong_ke":
+            include("thongke/list.php");
+            break;
+        default:
+        include("home.php");
+        break;
+    }
+}else{
+    include ("home.php");
+}
+
+
+include("footer.php");
+?>
---
+++ view/index.php
@@ -0,0 +1,206 @@
+<?php
+session_start();
+include "../model/pdo.php";
+include "../model/sanpham.php";
+include("../model/khac.php");
+include("../model/account.php");
+$thongbao = "";
+// session_destroy();
+
+if (!isset($_SESSION['my_cart'])) $_SESSION['my_cart']=[];
+$product = display_sanpham();
+$what_hot = display_what_hot();
+$new_product = display_new_product();
+$men_product = display_men_product();
+$men_featured = display_men_featured();
+$women_product = display_women_product();
+$women_featured = display_women_featured();
+$kid_product = display_kid_product();
+$kid_featured = display_kid_featured();
+$sale_product = display_sale_product();
+$sale_featured = display_sale_featured();
+include "header.php";
+if((isset($_GET['act'])) && ($_GET['act'])!=""){
+    $act = $_GET['act'];
+    switch ($act) {
+        case 'men':
+            include "men.php";
+            break;
+        case 'women':
+            include "women.php";
+            break;
+        case 'kid':
+            include "kid.php";
+            break;
+        case 'sale':
+            include "sale.php";
+            break;
+        case 'allsanpham':
+            if (isset($_POST["kw"]) && $_POST["kw"]!=""){
+                $kw = $_POST['kw'];
+            }else{
+                $kw = "";
+            }
+            $product_view = display_sanpham_view($kw);
+            include "allsanpham.php";
+            break;    
+        case 'sanphamct':
+            if (isset($_GET["product_id"]) && $_GET["product_id"]>0){
+                $product_id = $_GET['product_id'];
+                $update = load_1_san_pham($product_id);
+                foreach ($update as $up):
+                if ((isset($up['product_image2'])) && ($up['product_image2'])!="") {
+                    $sql = "SELECT * FROM color";
+                    $color = pdo_query($sql);
+                    $sql = "SELECT * FROM size";
+                    $size = pdo_query($sql);
+                    include "sanphamct.php";
+                }else{
+                    $sql = "SELECT * FROM color";
+                    $color = pdo_query($sql);
+                    $sql = "SELECT * FROM size";
+                    $size = pdo_query($sql);
+                    include "sanphamct2.php";
+                } 
+                endforeach;
+                
+            }
+            
+            // include "sanphamct.php";
+            break;
+        case 'cart':
+            if (isset($_POST["addtocart"]) && $_POST["addtocart"]){
+                $product_id = $_POST['product_id'];
+                $product_name = $_POST['product_name'];
+                $product_image = $_POST['product_image'];
+                $product_price = $_POST['product_price'];
+                $product_color = $_POST['color'];
+                $product_size = $_POST['size'];
+                $sl = $_POST['sl'];
+                $thanhtien = $product_price*$sl;
+                $spadd = [$product_id,$product_name,$product_price,$product_image,$sl,$thanhtien,$product_color,$product_size];
+                array_push($_SESSION['my_cart'],$spadd);
+            }
+            include "cart.php";
+            break;
+        case 'cart_home':
+            if($_SESSION['my_cart'] != []){
+                include "cart.php";
+            }else{
+                include "cart_rong.php";
+            }
+            break;
+        case 'delcart':
+            if(isset($_GET['idcart'])){
+                array_splice($_SESSION['my_cart'],$_GET['idcart'],1);
+            }else{
+                $_SESSION['my_cart']=[];
+            }
+            header('location: index.php?act=cart');
+            break;
+        case 'checkout':
+            if($_SESSION['my_cart'] != []){
+                include "checkout.php";
+            }else{
+                include "home.php";
+            }
+            
+            break;
+        case 'checkoutsuccess':
+            if (isset($_POST["checkout"]) && $_POST["checkout"]){
+                $receive_name = $_POST['receive_name'];
+                $receive_sdt = $_POST['receive_sdt'];
+                $receive_address_tinh = $_POST['receive_address_tinh'];
+                $receive_address_huyen = $_POST['receive_address_huyen'];
+                $receive_address_xa = $_POST['receive_address_xa'];
+                $receive_address_thon = $_POST['receive_address_thon'];
+                $receive_address = "$receive_address_thon, $receive_address_xa, $receive_address_huyen,        $receive_address_tinh";
+                $order_pttt = $_POST['order_pttt'];
+                $order_date = date('h:i:sa d/m/Y');
+                $totol = tongdonhang();
+                
+                $order_cart_id = insert_orderr($receive_name,$receive_address,$receive_sdt,$order_pttt,$order_date,$totol);
+
+
+                foreach(($_SESSION['my_cart']) as $cart ) {
+                    $product_id = $cart['0'];
+                    $product_name = $cart['1'];
+                    $product_price = $cart['2'];
+                    $product_image = $cart['3'];
+                    $product_quantity = $cart['4'];
+                    $product_thanhtien = $cart['5'];
+                    $product_color = $cart['6'];
+                    $product_size = $cart['7'];
+                    insert_oderr_cart($product_id,$product_quantity,$product_thanhtien,$product_color,$product_size,$order_cart_id);
+                }
+
+                $_SESSION['my_cart'] = [];
+            }
+            include "checkoutsuccess.php";
+            break;
+        case 'bill':
+            $order_cart_id = $_GET['order_cart_id'];
+            $orderr = select_orderr($order_cart_id);
+            $orderr_cart = select_orderr_cart($order_cart_id);
+            foreach ($orderr_cart as $orc){
+                $product_id = $orc['product_id'];
+                $orderr_cart_product = select_orderr_cart_product($product_id);
+            }
+            
+            include "bill.php";
+            break;
+        case 'account':
+            if (isset($_SESSION['user'])){
+                header('location: login_thanh_cong.php');
+            }else{
+            
+            include "login.php";
+            break;
+        }
+        case 'register':
+            if (isset($_POST["register"]) && $_POST["register"]){
+                $account_user = $_POST['account_user'];
+                $account_pass = $_POST['account_pass'];
+                $account_sdt = $_POST['account_sdt'];
+                $account_email = $_POST['account_email'];
+                $account_address = $_POST['account_address'];
+                register($account_user, $account_pass, $account_sdt, $account_email, $account_address);
+                $thongbao = "Chúc mừng bạn đã dăng ký thành công. Hãy đăng nhập để trải nghiệm";
+            }
+            
+            include "register.php";
+            break;
+        case 'login':
+            
+            if (isset($_POST['login']) && $_POST['login']){
+                $account_user = $_POST['account_user'];
+                $account_pass = $_POST['account_pass'];
+                $check_user = check_user($account_user,$account_pass);
+                if(is_array($check_user)){
+                    $_SESSION['user']=$check_user;
+                    header('location: index.php');
+                    
+                }else{
+                    $thongbao = "Tài khoản không tồn tại";
+                }
+            }
+            if (isset($_SESSION['user'])){
+                include "login_thanh_cong.php";
+            }else{
+            
+            include "login.php";
+            break;
+        }
+        case 'logout':
+            if (isset($_POST['logout']) && $_POST['logout']){
+            session_destroy();
+        }
+            header('location: index.php');
+            break;
+        default:
+            include "home.php";
+            break;
+    }
+}else include "home.php";
+include "footer.php";
+?>
